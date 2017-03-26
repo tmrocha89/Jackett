6c0aad5e829e6b7db4793e3ddc208ff58cf1c834
Tiago Rocha <tmrocha89@gmail.com>
2017-03-26 02:13:29 +0100

    Categories added. Torrent's info added

--- src/Jackett/Indexers/YTS.cs
+++ src/Jackett/Indexers/YTS.cs
@@ -12,7 +12,6 @@ using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Text;
-using System.Web;
 
 namespace Jackett.Indexers
 {
@@ -34,13 +33,6 @@ namespace Jackett.Indexers
             set { base.configData = value; }
         }
 
-        private DateTime lastTokenFetch;
-        private string token;
-
-        readonly TimeSpan TOKEN_DURATION = TimeSpan.FromMinutes(10);
-
-        private bool HasValidToken { get { return !string.IsNullOrEmpty(token) && lastTokenFetch > DateTime.Now - TOKEN_DURATION; } }
-
         public YTS(IIndexerManagerService i, IWebClient wc, Logger l, IProtectionService ps)
             : base(name: "YTS",
                 description: null,
@@ -60,45 +52,10 @@ namespace Jackett.Indexers
 
             webclient.requestDelay = 2; // 0.5 requests per second
 
-            /*
-            AddCategoryMapping(4, TorznabCatType.XXX, "XXX (18+)");
-            AddCategoryMapping(14, TorznabCatType.MoviesSD, "Movies/XVID");
-            AddCategoryMapping(48, TorznabCatType.MoviesHD, "Movies/XVID/720");
-            AddCategoryMapping(17, TorznabCatType.MoviesSD, "Movies/x264");
-            AddCategoryMapping(44, TorznabCatType.MoviesHD, "Movies/x264/1080");
-            AddCategoryMapping(45, TorznabCatType.MoviesHD, "Movies/x264/720");
-            AddCategoryMapping(47, TorznabCatType.Movies3D, "Movies/x264/3D");
-            AddCategoryMapping(42, TorznabCatType.MoviesBluRay, "Movies/Full BD");
-            AddCategoryMapping(46, TorznabCatType.MoviesBluRay, "Movies/BD Remux");
-            AddCategoryMapping(18, TorznabCatType.TVSD, "TV Episodes");
-            AddCategoryMapping(41, TorznabCatType.TVHD, "TV HD Episodes");
-            AddCategoryMapping(23, TorznabCatType.AudioMP3, "Music/MP3");
-            AddCategoryMapping(25, TorznabCatType.AudioLossless, "Music/FLAC");
-            AddCategoryMapping(27, TorznabCatType.PCGames, "Games/PC ISO");
-            AddCategoryMapping(28, TorznabCatType.PCGames, "Games/PC RIP");
-            AddCategoryMapping(40, TorznabCatType.ConsolePS3, "Games/PS3");
-            AddCategoryMapping(32, TorznabCatType.ConsoleXbox360, "Games/XBOX-360");
-            AddCategoryMapping(33, TorznabCatType.PCISO, "Software/PC ISO");
-            AddCategoryMapping(35, TorznabCatType.BooksEbook, "e-Books");
-            */
+            AddCategoryMapping(3, TorznabCatType.Movies3D, "3D");
+            AddCategoryMapping(720, TorznabCatType.MoviesHD, "720p");
+            AddCategoryMapping(1080, TorznabCatType.MoviesHD, "1080p");
         }
-
-
-        /*async Task CheckToken()
-        {
-            if (!HasValidToken)
-            {
-                var queryCollection = new NameValueCollection();
-                queryCollection.Add("get_token", "get_token");
-
-                var tokenUrl = ApiEndpoint + "?" + queryCollection.GetQueryString();
-
-                var result = await RequestStringWithCookiesAndRetry(tokenUrl);
-                var json = JObject.Parse(result.Content);
-                token = json.Value<string>("token");
-                lastTokenFetch = DateTime.Now;
-            }
-        }*/
 
         public async Task<IndexerConfigurationStatus> ApplyConfiguration(JToken configJson)
         {
@@ -122,42 +79,45 @@ namespace Jackett.Indexers
         {
             var release = new ReleaseInfo();
             release.Title = jsonData.Value<string>("title");
-            release.Link = new Uri(jsonData.Value<string>("url"));
             release.Description = jsonData.Value<string>("description_full");
+            release.Comments = new Uri(jsonData.Value<string>("url"));
+            string imdbId = jsonData.Value<string>("imdb_code");
+            if (!String.IsNullOrEmpty(imdbId))
+            {
+                release.Imdb = ParseUtil.GetImdbID(imdbId);
+            }
+            
+            ;
+            return release;
+        }
+
+        private ReleaseInfo getTorrentsInformation(JToken data, ReleaseInfo release)
+        {
+            release.PublishDate = DateTime.ParseExact(data.Value<string>("date_uploaded"), "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
+            release.Seeders = data.Value<int>("seeds");
+            release.Peers = data.Value<int>("peers") + release.Seeders;
+            release.Size = data.Value<long>("size_bytes");
+            release.InfoHash = data.Value<string>("hash");
+            release.Link = new Uri(data.Value<string>("url"));
+            release.Category = MapTrackerCatDescToNewznab(data.Value<string>("quality"));
+            release.DownloadVolumeFactor = 0;
+            release.UploadVolumeFactor = 1;
             return release;
         }
 
         public async Task<IEnumerable<ReleaseInfo>> PerformQuery(TorznabQuery query, int attempts = 0)
         {
-            //await CheckToken();
             var releases = new List<ReleaseInfo>();
             var searchString = query.GetQueryString();
             
             var queryCollection = new NameValueCollection();
             queryCollection.Add("query_term", query.SearchTerm);
-            /*
-            queryCollection.Add("token", token);
-            queryCollection.Add("format", "json_extended");
-            queryCollection.Add("app_id", "jackett_v" + Engine.ConfigService.GetVersion());
-            queryCollection.Add("limit", "100");
-            queryCollection.Add("ranked", "0");
-            */
 
             if (query.ImdbID != null)
             {
                 queryCollection.Add("mode", "search");
                 queryCollection.Add("search_imdb", query.ImdbID);
             }
-            else if (query.RageID != null)
-            {
-                queryCollection.Add("mode", "search");
-                queryCollection.Add("search_tvrage", query.RageID.ToString());
-            }
-            /*else if (query.TvdbID != null)
-            {
-                queryCollection.Add("mode", "search");
-                queryCollection.Add("search_tvdb", query.TvdbID);
-            }*/
             else if (!string.IsNullOrWhiteSpace(searchString))
             {
                 queryCollection.Add("mode", "search");
@@ -191,39 +151,14 @@ namespace Jackett.Indexers
 
                 foreach (var item in jsonData.Value<JArray>("movies"))
                 {
-                    var release = getMovieBasicInformation(item); //new ReleaseInfo();
-                    /*release.Title = item.Value<string>("title");
-                    release.Category = MapTrackerCatDescToNewznab(item.Value<string>("category"));
+                    var release = getMovieBasicInformation(item);
 
-                    release.MagnetUri = new Uri(item.Value<string>("download"));
-                    release.InfoHash = release.MagnetUri.ToString().Split(':')[3].Split('&')[0];
-
-                    release.Comments = new Uri(item.Value<string>("info_page"));
-                    release.Guid = release.Comments;
-
-                    var episode_info = item.Value<JToken>("episode_info");
-
-                    if (episode_info.HasValues)
+                    var torrentsList = item.Value<JArray>("torrents");
+                    foreach(JToken torrentItem in torrentsList)
                     {
-                        var imdb = episode_info.Value<string>("imdb");
-                        release.Imdb = ParseUtil.GetImdbID(imdb);
-                        release.TVDBId = episode_info.Value<long?>("tvdb");
-                        release.RageID = episode_info.Value<long?>("tvrage");
-                        release.TMDb = episode_info.Value<long?>("themoviedb");
+                        releases.Add(getTorrentsInformation(torrentItem, (ReleaseInfo)release.Clone()));
                     }
-
-                    // ex: 2015-08-16 21:25:08 +0000
-                    var dateStr = item.Value<string>("pubdate").Replace(" +0000", "");
-                    var dateTime = DateTime.ParseExact(dateStr, "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
-                    release.PublishDate = DateTime.SpecifyKind(dateTime, DateTimeKind.Utc).ToLocalTime();
-
-                    release.Seeders = item.Value<int>("seeders");
-                    release.Peers = item.Value<int>("leechers") + release.Seeders;
-                    release.Size = item.Value<long>("size");
-                    release.DownloadVolumeFactor = 0;
-                    release.UploadVolumeFactor = 1;
-                    */
-                    releases.Add(release);
+                    
                 }
             }
             catch (Exception ex)
